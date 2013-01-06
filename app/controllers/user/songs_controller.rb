fa9ccc36a940b1e587de1938f54d2731f47520c3
Ferris Tseng <ferristseng@ferris.home>
2013-01-05 20:45:00 -0500

    revised pagination for songs

--- app/controllers/user/songs_controller.rb
+++ app/controllers/user/songs_controller.rb
@@ -24,20 +24,46 @@ class User::SongsController < ApplicationController
 
   def search
     @station = params[:station_id]
-    max_results = Settings['station']['max_results'].to_i + 10
+    max_results = Settings['station']['max_results'].to_i
   	base_url = "http://ex.fm/api/v3/song/search/"
-  	search_url = "#{base_url}#{URI.encode(params[:query])}?results=#{max_results}&start=#{(params[:start].to_i - 1) * max_results}"
+  	search_url = "#{base_url}#{URI.encode(params[:query])}?results=#{max_results + 10}&start=#{(params[:start].to_i - 1) * max_results}"
   	raw_content  = open(search_url) {|f| f.read }
   	json = ActiveSupport::JSON.decode raw_content
   	@songs = json['songs'][0..15]
-    # pagination
-  	@pages = json['total'] / max_results
-  	@current = json['start'] / max_results
-    @page = (params[:start].to_i > 1) ? @current - 1 : @current
-    @max_page = (@pages > max_results) ? @current + 4 : @current
+    paginate_results(json['total'], json['start'], max_results)
     # query and song params
     @query = params[:query]
     @song = Song.new
   end
+
+  private
+
+    def paginate_results(total_pages, start_page, max_results)
+      x = 5
+
+      offset = (x / 2).floor
+      center = offset + 1
+
+      @m_page     = (total_pages / max_results).floor + 1
+      @c_page     = (start_page / max_results).floor + 1
+      @s_page     = 0
+      @e_page     = 0
+
+      # Set the end page
+      if @c_page + offset > @m_page
+        @s_page = @m_page - x + 1
+        @e_page = @m_page 
+      else
+        @e_page = @c_page + offset
+      end
+
+      # Set the start page
+      if @c_page > offset && @s_page == 0
+        @s_page = @c_page - offset
+      elsif @s_page == 0
+        @s_page = @c_page - (@c_page - 1)
+        @e_page = @c_page + (x - @c_page)
+      end
+    end
 
 end
