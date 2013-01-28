8d61a523ac90e33059b1aaa8d9ebf0073820c10f
Ferris Tseng <ferristseng@ferris.local>
2013-01-28 13:24:18 -0500

    updated bug with link_to_favorite and non logged-in users

--- app/helpers/stations_helper.rb
+++ app/helpers/stations_helper.rb
@@ -1,14 +1,16 @@
 module StationsHelper
 
 	def link_to_favorite(station, label = { :favorite => "Favorite", :unfavorite => "Unfavorite" })
-		favorited = current_user.favorited?(station)
-		raw link_to(
-					favorited ? label[:unfavorite] : label[:favorite], 
-					user_station_favorite_path(station), 
-					:method => :post,
-					:remote => true,
-					:class => "favorite-link label #{favorited ? 'red' : 'yellow'}"
-				)
+		if signed_in?
+			favorited = current_user.favorited?(station)
+			raw link_to(
+						favorited ? label[:unfavorite] : label[:favorite], 
+						user_station_favorite_path(station), 
+						:method => :post,
+						:remote => true,
+						:class => "favorite-link label #{favorited ? 'red' : 'yellow'}"
+					)
+		end
 	end
 
 	def station_label(station)
