a37193348a84e2c95e77c5cb487e73e9811aef4e
Ferris Tseng <ferristseng@ferris.home>
2013-06-27 14:58:47 -0400

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+Group.create(:name => 'Admin', :permission => 9)
+Group.create(:name => 'Default', :permission => 0)
